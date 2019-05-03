c04752cd67d04155da939e292e47c494b4f93bf0
soyong20161205 <soyong20161205@gmail.com>
2019-05-03 21:18:42 +0700

    Integrate react-to-print third party library #70

--- src/components/Pages/Invoice.js
+++ src/components/Pages/Invoice.js
@@ -1,60 +1,50 @@
 import React from 'react'
-class Invoice extends React.Component {
-  render() {
-    return (
-        <main id="main" role="main">
-            <h1>
-                Invoice
-                <button className="ml-auto btn btn-success btn-md float-right " type="submit">
-                    <i className="fas fa-plus"></i>
-                    Add Invoice
-                </button>
-            </h1>
-            
+import ReactToPrint from 'react-to-print';
+
+class InvoiceContent extends React.Component {
+    render () {
+        return (
             <div className="card">
                 <div className="card-body">
                     <div className="d-flex align-items-center">
                         <h3 className="m-0">Invoice 00004879</h3>
-                        <button className="ml-auto btn btn-secondary btn-sm" type="button">
-                            Copy this invoice
-                        </button>
                     </div>
                     <hr />
                     <div className="row mb-3">
-                        <div className="col-xl-4 col-6 br py-2">
-                            <div className="row">
-                                <div className="col-lg-4 text-center d-none d-lg-block">
-                                    <em className="fa fa-plane fa-4x text-muted"></em>
+                        <div className="col-sm-4 col-4 br py-2">
+                            <div className="row flex-vertical-align-center">
+                                <div className="col-lg-4 d-none text-center d-lg-block">
+                                    <em className="fa fa-plane fa-3x text-muted"></em>
                                 </div>
-                                <div className="col-lg-8">
+                                <div className="col-lg-8 col-12">
                                     <h4>Bill Mitchelle</h4>
-                                    <address></address>Nowhere 1024<br />Happy St., 50487<br />Neverland
+                                    <address>Nowhere 1024<br />Happy St., 50487<br />Neverland</address>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-xl-4 col-6 br py-2">
-                            <div className="row">
-                                <div className="col-lg-4 text-center d-none d-lg-block">
-                                    <em className="fa fa-truck fa-4x text-muted"></em>
+                        <div className="col-sm-4 col-4 br py-2">
+                            <div className="row flex-vertical-align-center">
+                                <div className="col-lg-4 d-none text-center d-lg-block">
+                                    <em className="fa fa-truck fa-3x text-muted"></em>
                                 </div>
-                                <div className="col-lg-8">
-                                    <h4>Lloyd Gonzales</h4>
-                                    <address></address>Basilisa 2145<br />Mate, Cliff., 20145<br />Moon
+                                <div className="col-lg-8 col-12">
+                                    <h4>Lloyd Terrel</h4>
+                                    <address>Basilisa 2145<br />Mate, Cliff., 20145<br />Moon</address>
                                 </div>
                             </div>
                         </div>
-                        <hr />
-                        <div className="col-xl-4 col-12 py-2">
+                        {/* <hr className="d-sm-none" style={{ "width":"100%" }}/> */}
+                        <div className="col-sm-4 col-4 py-2">
                             <div className="clearfix">
-                                <p className="float-left">INVOICE NO.</p>
+                                <h5 className="float-left">INVOICE NO.</h5>
                                 <p className="float-right mr-2">00004879</p>
                             </div>
                             <div className="clearfix">
-                                <p className="float-left">Date</p>
+                                <h5 className="float-left">Date</h5>
                                 <p className="float-right mr-2">25/08/2014</p>
                             </div>
                             <div className="clearfix">
-                                <p className="float-left">Due Date</p>
+                                <h5 className="float-left">Due Date</h5>
                                 <p className="float-right mr-2">30/08/2014</p>
                             </div>
                         </div>
@@ -96,6 +86,7 @@ class Invoice extends React.Component {
                             </tbody>
                         </table>
                     </div>
+                    <hr />
                     <div className="row">
                         <div className="col-lg-6">
                         </div>
@@ -122,13 +113,42 @@ class Invoice extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <hr className="d-print-none" />
-                    <div className="clearfix">
-                        <button className="btn btn-primary float-left mr-2" type="button">Edit</button>
-                        <button className="btn btn-orange float-left" type="button">Print</button>
-                        <button className="btn btn-success float-right" type="button">Send Invoice</button>
-                    </div>
                 </div>
+            </div>
+        )
+    }
+}
+
+class Invoice extends React.Component {
+  render() {
+    return (
+        <main id="main" role="main">
+            <div style={{ "display":"flex", "justifyContent" : "space-between", "flexWrap": "wrap"}}>
+                <h1 style={{ "border": "none", "marginBottom": "none" }}>
+                    Invoice
+                </h1>
+                <div>
+                    <button className="m-1 btn btn-success btn-md float-right" type="button">
+                        <i className="fas fa-copy"></i>
+                        &nbsp;Copy this invoice
+                    </button>
+                    <button className="m-1 btn btn-success btn-md float-right " type="submit">
+                        <i className="fas fa-plus"></i>
+                        &nbsp;Add Invoice
+                    </button>
+                </div>
+            </div>
+            <InvoiceContent ref={el => (this.componentRef = el)} />
+
+            {/* <hr className="d-print-none" /> */}
+            <div className="clearfix">
+                <button className="btn btn-primary float-left mr-2" type="button">Edit</button>
+                <ReactToPrint
+                    trigger={() => <button className="btn btn-orange float-left" type="button">Print</button>}
+                    content={() => this.componentRef}
+                    copyStyles='true'
+                />
+                <button className="btn btn-success float-right" type="button">Send Invoice</button>
             </div>
         </main>
     )
