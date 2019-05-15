551a153637c021f2e5f9b23c87caa107a16aceeb
soyong20161205 <soyong20161205@gmail.com>
2019-05-15 14:42:38 +0700

    #71 done, #74 done

---
+++ src/components/Modals/Modal2.js
@@ -0,0 +1,106 @@
+import React from 'react'
+import ReactModal from 'react-modal'
+import './Styles.css';
+
+class Modal2 extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+          showModal: false
+        };
+        
+        this.handleOpenModal = this.handleOpenModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
+      }
+      
+      handleOpenModal () {
+        this.setState({ showModal: true });
+      }
+      
+      handleCloseModal () {
+        this.setState({ showModal: false });
+      }
+      
+      render () {
+        return (
+            <div className="container-fluid">
+                <div className="d-flex align-items-stretch">
+                    <main id="main" role="main">
+                        <h1>Modal(This modal will be closed if you click the anywhere outside the modal.)</h1>
+                        <div>
+                            <button onClick={this.handleOpenModal} className="btn btn-primary">Trigger Modal</button>
+                            <ReactModal 
+                                isOpen={this.state.showModal}
+                                contentLabel="onRequestClose Example"
+                                onRequestClose={this.handleCloseModal}
+                                shouldCloseOnOverlayClick={true}
+                                className="Modal"
+                                overlayClassName="Overlay"
+                            >
+                                <div className="modal-content">
+
+                                    <div className="modal-header">
+                                        <h4 className="modal-title" id="myModalLabel">Your cart</h4>
+                                        <button type="button" className="close" onClick={this.handleCloseModal}>
+                                        <span aria-hidden="true">×</span>
+                                        </button>
+                                    </div>
+
+                                    <div className="modal-body">
+                                        <table className="table table-hover">
+                                            <thead>
+                                                <tr>
+                                                <th>#</th>
+                                                <th>Product name</th>
+                                                <th>Price</th>
+                                                <th>Remove</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                <th scope="row">1</th>
+                                                <td>Product 1</td>
+                                                <td>100$</td>
+                                                <td><a><i class="fas fa-times"></i></a></td>
+                                                </tr>
+                                                <tr>
+                                                <th scope="row">2</th>
+                                                <td>Product 2</td>
+                                                <td>100$</td>
+                                                <td><a><i class="fas fa-times"></i></a></td>
+                                                </tr>
+                                                <tr>
+                                                <th scope="row">3</th>
+                                                <td>Product 3</td>
+                                                <td>100$</td>
+                                                <td><a><i class="fas fa-times"></i></a></td>
+                                                </tr>
+                                                <tr>
+                                                <th scope="row">4</th>
+                                                <td>Product 4</td>
+                                                <td>100$</td>
+                                                <td><a><i class="fas fa-times"></i></a></td>
+                                                </tr>
+                                                <tr class="total">
+                                                <th scope="row">5</th>
+                                                <td>Total</td>
+                                                <td>400$</td>
+                                                <td></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-outline-primary" onClick={this.handleCloseModal}>Close</button>
+                                        <button className="btn btn-primary">Checkout</button>
+                                    </div>
+                                </div>
+                            </ReactModal>
+                         </div>
+                    </main>
+                </div>
+            </div>
+        );
+    }
+}
+export default Modal2
--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,13 @@ import React from 'react'
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <div className="container-fluid">
+        <div className="d-flex align-items-stretch">
+            <main id="main" role="main">
+              <h1>Home</h1>
+            </main>
+        </div>
+    </div>
     )
   }
 }
